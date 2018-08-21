67aca74a89bca2a449f7e864b69aa11e1b8b66f0
Choopong Choosamer <choopong@central.tech>
2018-08-21 17:10:32 +0700

    make delivery report working

--- producer.js
+++ producer.js
@@ -1,14 +1,13 @@
 const Kafka = require('node-rdkafka');
 
 let numSent = 0;
-let numDelivered = 0;
 
 function onLog(log) {
   console.log(log);
 }
 
 function onDR(err, report) {
-  console.log('delivery-report: ' + JSON.stringify(report));
+  console.log('prodcuced: ' + report.key.toString('utf8'));
 }
 
 function onError(err) {
@@ -22,12 +21,12 @@ function onReady(arg) {
 }
 
 function sendMessage() {
+  producer.poll();
   numSent++;
   let message = Buffer.from('Message # ' +numSent);
   // if partition is set to -1, librdkafka will use the default partitioner
   let partition = -1;
-  producer.produce(topicName, partition, message);
-  console.log('prodcuced: ' + message);
+  producer.produce(topicName, partition, message, 'key-'+numSent);
   setTimeout(sendMessage, 1000);
 }
 
@@ -48,17 +47,10 @@ producer.on('event.log', onLog);
 //logging all errors
 producer.on('event.error', onError);
 
-// producer.on('delivery-report', onDR);
-
-producer.on('delivery-report', function(err, report) {
-  console.log('delivery-report: ' + JSON.stringify(report));
-  counter++;
-});
+producer.on('delivery-report', onDR);
 
 //Wait for the ready event before producing
 producer.on('ready', onReady);
-
-producer.on('disconnected', onDisconnect);
 
 //starting the producer
 producer.connect();
